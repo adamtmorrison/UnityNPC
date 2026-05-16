86e23b4732a4058170746dfd35f3fb649c87fd5f
Adam T. Morrison <38388951+adamtmorrison@users.noreply.github.com>
2026-05-15 19:47:54 -0500

    Add files via upload
    Updated NPC ID info.

--- data/npcs.lua
+++ data/npcs.lua
@@ -26,13 +26,13 @@ end
 local Npcs = {}
 
 Npcs.Values = {}
-Npcs.Values['']       = { id = 00000000, en = '',                 zone = 000 }
+Npcs.Values['']       = { id = 00000000, en = '',                 	zone = 000 }
 
-Npcs.Values[17739961] = { id = 17739961, en = 'Igsli',            zone = 235 }
-Npcs.Values[17719646] = { id = 17719646, en = 'Urbiolaine',       zone = 230 }
-Npcs.Values[17764611] = { id = 17764611, en = 'Teldro-Kesdrodo',  zone = 241 }
-Npcs.Values[17764612] = { id = 17764612, en = 'Yonolala',         zone = 241 }
-Npcs.Values[17826181] = { id = 17826181, en = 'Nunaarl Bthtrogg', zone = 256 }
+Npcs.Values[17739958] = { id = 17739958, en = 'Igsli', 				zone = 235 }
+Npcs.Values[17719643] = { id = 17719643, en = 'Urbiolaine', 		zone = 230 }
+Npcs.Values[17764608] = { id = 17764608, en = 'Teldro-Kesdrodo', 	zone = 241 }
+Npcs.Values[17764609] = { id = 17764609, en = 'Yonolala', 			zone = 241 }
+Npcs.Values[17826178] = { id = 17826178, en = 'Nunaarl Bthtrogg', 	zone = 256 }
 
 --------------------------------------------------------------------------------
 function Npcs.GetByProperty(key, value)
